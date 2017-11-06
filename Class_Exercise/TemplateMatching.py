7c9437be7839b8bd6749a5c7c3df8572bcd37989
GrazianoMJ <GrazianoMJ@users.noreply.github.com>
2017-11-06 18:29:14 -0500

    Object Template Scan Exercise * Added several images to the Class_Exercise folder:   - source_img.jpg   - source_img1.jpg   - template.jpg   - template1.jpg   - RAW_IMAGE.jpg * Added code to the TemplateMatching.py file that calculates the appropriate mean and variance values, slides the template image across the source to find the location of the template, draws a blue box around the image and saves it as match_img.jpg

--- Class_Exercise/TemplateMatching.py
+++ Class_Exercise/TemplateMatching.py
@@ -6,7 +6,8 @@ def TemplateMatching(src, temp, stepsize): # src: source image, temp: template i
     var_t = 0;
     location = [0, 0];
     # Calculate the mean and variance of template pixel values
-    # ------------------ Put your code below ------------------ 
+    mean_t = np.mean(temp)
+    var_t = np.var(temp)
                     
     max_corr = 0;
     # Slide window in source image and find the maximum correlation
@@ -16,11 +17,15 @@ def TemplateMatching(src, temp, stepsize): # src: source image, temp: template i
             var_s = 0;
             corr = 0;
             # Calculate the mean and variance of source image pixel values inside window
-            # ------------------ Put your code below ------------------ 
+            cropped_src = src[i:(i+temp.shape[0]), j:(j+temp.shape[1])]
+            mean_s = np.mean(cropped_src)
+            var_s = np.var(cropped_src)
             
             # Calculate normalized correlation coefficient (NCC) between source and template
-            # ------------------ Put your code below ------------------ 
-            
+            for k in np.arange(0, temp.shape[0]):
+                for l in np.arange(0, temp.shape[1]):
+                    corr += 1/float(temp.shape[0]*temp.shape[1])*(temp[k,l]-mean_t)*(cropped_src[k,l]-mean_s)/(var_t*var_s)
+
             if corr > max_corr:
                 max_corr = corr;
                 location = [i, j];
@@ -34,10 +39,13 @@ print(location)
 match_img = cv2.cvtColor(source_img, cv2.COLOR_GRAY2RGB)
 
 # Draw a red rectangle on match_img to show the template matching result
-# ------------------ Put your code below ------------------ 
+cv2.rectangle(match_img, (location[1], location[0]), 
+              (location[1]+temp.shape[1]-1, location[0]+temp.shape[0]-1),
+              (255,0,0), 5 
+)
 
 # Save the template matching result image (match_img)
-# ------------------ Put your code below ------------------ 
+cv2.imwrite('match_img.jpg', match_img)
 
 
 # Display the template image and the matching result
